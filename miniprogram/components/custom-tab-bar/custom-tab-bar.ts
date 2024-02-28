46d111ae38967e198d19c89a9d887634de1ee349
BENZJ <jbcbenz@gmail.com>
2024-02-28 22:23:25 +0800

    feat: 首次提交

---
+++ miniprogram/components/custom-tab-bar/custom-tab-bar.ts
@@ -0,0 +1,67 @@
+// components/custom-tab-bar.ts
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    // 组件的内部数据
+    active: 0,
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        text: "首页",
+        // iconPath: "./assets/home.png",
+        // selectedIconPath: "./assets/home-active.png"
+      },
+      {
+        pagePath: "/pages/logs/logs",
+        text: "我的",
+        // iconPath: "./assets/user.png",
+        // selectedIconPath: "./assets/user-active.png"
+      }
+    ]
+  },
+
+  pageLifetimes: {
+    show() {
+      this.updateActiveTab();
+    }
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    onChange(event: any) {
+      // console.log(event.currentTarget.dataset.index);
+      const url = this.data.list[event.currentTarget.dataset.index].pagePath;
+      wx.switchTab({ url });
+      this.setData({ active: event.currentTarget.dataset.index});
+    },
+    updateActiveTab() {
+      // 获取当前页面栈
+      const pages = getCurrentPages();
+      console.log(pages)
+      const currentPage = pages[pages.length - 1].route;
+      console.log(currentPage)
+      
+      // 匹配当前页面与tab列表，更新active状态
+      const list = this.data.list;
+      const activeIndex = list.findIndex(item => `/${currentPage}` === item.pagePath);
+      if (activeIndex !== -1) {
+        this.setData({ active: activeIndex });
+      }
+    },
+    init() {
+      // 初始化数据
+    }
+  }
+})
